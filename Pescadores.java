240587e3693933083c290495a2326fffca4b2353
João Muniz Neto <43187591+JMNeto28@users.noreply.github.com>
2019-11-25 18:05:48 -0300

    Update Pescadores.java

--- Pescadores.java
+++ Pescadores.java
@@ -1,11 +1,31 @@
 package pesca;
 
-public class Pescadores {
-  
-	int tipopescador;
-	int vip;
-	int conta;
-	
+import java.util.Scanner;
 
+public class Pescadores extends Cadastro {
+	int tip, tip1;
+	void regis1()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Realize o cadastro do pescador:");
+		System.out.println("Digite o nome:");
+		this.nome = sc.nextLine();
+		System.out.println("Digite o CPF:");
+		this.cpf = sc.nextLine();
+		System.out.println("Digite o endereço:");
+		this.endereço = sc.nextLine();
+		System.out.println("Digite o numero:");
+		this.numero = sc.nextLine();
+		System.out.println("Digite o tipo de pescador (em breve):");
+		tip = sc.nextInt();
+		sc.nextLine();
+		this.setTipoPescador(tip);
+		System.out.println("Digite 1 se o cliente for VIP ou 2 se não for VIP:");
+		tip1 = sc.nextInt();
+		sc.nextLine();
+		this.setVip(tip1);
 	}
+}
+
+
 
