99113f86c4a321094ff8abd6e806dfdeed7cf50c
João Muniz Neto <43187591+JMNeto28@users.noreply.github.com>
2019-11-25 17:57:16 -0300

    Update Aluguel.java

--- Aluguel.java
+++ Aluguel.java
@@ -3,41 +3,63 @@ package pesca;
 import java.util.Scanner;
 
 public class Aluguel extends Carro {
-	
 	Scanner sc = new Scanner(System.in);
-	int esc, conta;
-		int AluguelCarro(NotaFiscal nota1) 
+	int esc;
+	double som,conta;
+
+		public double somar(double aux) 
 		{
+		
+			if (aux == 1)
+			{
+				System.out.println("O carro basico foi escolhido.");
+				this.setAr(300); 
+				this.setDirecaoEletrica(200);
+				som = this.getAr() + this.getDirecaoEletrica();
+
+			}
+			if (aux == 2)
+			{
+				System.out.println("O médio foi escolhido.");
+				this.setAr(300); 
+				this.setDirecaoEletrica(200);
+				this.setAlarme(400);
+				this.setSeguro(150);
+				som = this.getAr() + this.getDirecaoEletrica() + this.getAlarme() + this.getSeguro();
+			}
+			if (aux == 3)
+			{
+				System.out.println("O luxo basico foi escolhido.");
+				this.setAr(300); 
+				this.setDirecaoEletrica(200);
+				this.setAlarme(400);
+				this.setSeguro(150);
+				this.setSeguroTotal(350);
+				som = this.getAr() + this.getDirecaoEletrica() + this.getAlarme() + this.getSeguro() + this.getSeguroTotal();
+			}
+			return som;
+
+		}
+		int AluguelCarro(){
+			System.out.println("Escolha seu tipo de carro para o aluguel.\n1- Básico\n2- Médio\n3- Luxo");
+			
 			esc = sc.nextInt();
 			sc.nextLine();
-			System.out.println("Escolha seu tipo de carro para o aluguel.\n1- Básico\n2- Médio\n3- Luxo");
 			if (esc == 1)
 			{
 				System.out.println("O carro basico foi escolhido.");
-				this.ar = 300; 
-				this.direcaoeletrica = 200;
-				nota1.aluguel = this.ar + this.direcaoeletrica;
+
 			}
 			else if (esc == 2)
 			{
 				System.out.println("O médio basico foi escolhido.");
-				this.ar = 300; 
-				this.direcaoeletrica = 200;
-				this.alarme = 400;
-				this.seguro = 150;
-				nota1.aluguel = this.ar + this.direcaoeletrica + this.alarme + this.seguro;
+				
 			}
-			else
-			{
-				System.out.println("O luxo basico foi escolhido.");
-				this.ar = 300; 
-				this.direcaoeletrica = 200;
-				this.alarme = 400;
-				this.seguro = 150;
-				this.segurototal = 350;
-				nota1.aluguel = this.ar + this.alarme + this.alarme + this.seguro + this.segurototal;
-			}
-			return conta;
-
+			else System.out.println("O carro de luxo foi escolhido.");
+		
+			return esc;
 		}
+		
+		
+	
 }
