93005b9a57d3a341394782837c7f09015f9758df
João Muniz Neto <43187591+JMNeto28@users.noreply.github.com>
2019-11-27 13:29:56 -0300

    Update NotaFiscal.java

--- NotaFiscal.java
+++ NotaFiscal.java
@@ -7,20 +7,24 @@ public class NotaFiscal {
 	double aluguel;
 	int translado;
 	
-	//ArrayList<NotaFiscal> listaNotas = new ArrayList<NotaFiscal>();
 	
 	double Relatorio() {
 		
 		return pacote;
 	}
 	
-	void Dados(Pescadores cadas, String local)
+	void Dados(Pescadores cadas, String local, Cadastro cad)
 	{
 		System.out.println("Nome: " + cadas.nome + "\n");
+		System.out.println("Tipo de pescador:\n");
+		if(cad.getTipoPescador() == 1) System.out.println("Pescador esportivo.\n");
+		else if(cad.getTipoPescador() == 2) System.out.println("Pescador amador.\n");
+		else System.out.println("Pescador profissional.\n");
 		System.out.println("Local escolhido: " + local + "\n");
 		
 	}
-	
+
+
 
 
 }
