df42b67053db3dfd49eaa67505a8200bcd340ab4
João Muniz Neto <43187591+JMNeto28@users.noreply.github.com>
2019-11-27 13:31:05 -0300

    Update Pescadores.java

--- Pescadores.java
+++ Pescadores.java
@@ -16,13 +16,11 @@ public class Pescadores extends Cadastro {
 		this.endereço = sc.nextLine();
 		System.out.println("Digite o numero:");
 		this.numero = sc.nextLine();
-		System.out.println("Digite o tipo de pescador (em breve):");
+		System.out.println("Digite o tipo de pescador:1- Esportivo\n2- Amador\n3- Profissional\n");
 		tip = TraExce.lerInt();
-	
 		this.setTipoPescador(tip);
 		System.out.println("Digite 1 se o cliente for VIP ou 2 se não for VIP:");
 		tip1 = TraExce.lerInt();
-	
 		this.setVip(tip1);
 	}
 }
