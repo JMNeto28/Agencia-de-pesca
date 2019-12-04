8dd522fbcbe36187b7d72048d44bb95c7780fe41
João Muniz Neto <43187591+JMNeto28@users.noreply.github.com>
2019-12-04 09:26:56 -0300

    Update Pesca.java

--- Pesca.java
+++ Pesca.java
@@ -1,10 +1,11 @@
 package pesca;
 
+import java.text.ParseException;
 import java.util.Scanner;
 
 public class Pesca{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		Pescadores p1 = new Pescadores();
 		Pacote pacote = new Pacote();
 		NotaFiscal nota = new NotaFiscal();
@@ -61,7 +62,7 @@ public class Pesca{
 				System.out.println("Nota fiscal para o cliente:");
 				if (e == 1) somadou = (nota.Relatorio()+ carro.somar(aux));
 				else somadou = (nota.Relatorio() + carro1.somar(aux));
-				nota.Dados(p1, LocalEscolhido, cadastro);
+				nota.Dados(p1, LocalEscolhido);
 				System.out.printf("Valor bruto: R$%.2f\n",  somadou);
 				System.out.printf("Valor com desconto: R$%.2f\n", valor);
 				
